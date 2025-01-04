0ca928dfa772e87c3db66a14312c890c1ee4a9b4
Emmanuel FAIVRE <manu@manucorp.com>
2025-01-04 18:03:38 +0100

    export pour github pages

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.NODE_ENV !== "development"?"/uv2-tirage":"",
-    output: 'standalone',  // <=== enables static exports
+    output: 'export',  // <=== enables static exports
     reactStrictMode: true,      // Enable React strict mode for improved error handling
     compiler: {
         removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
