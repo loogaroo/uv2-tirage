0fdeaee62b7d10604929bb61ce8619b8d7ce8518
Emmanuel FAIVRE <manu@manucorp.com>
2025-01-04 16:35:30 +0100

    push github action

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,11 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/uv2-tirage",
+    output: "export",  // <=== enables static exports
     reactStrictMode: true,      // Enable React strict mode for improved error handling
     //swcMinify: true,            // Enable SWC minification for improved performance
     compiler: {
         removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
     }
 };
-
-export default withPWA({
-    dest: "public",         // destination directory for the PWA files
-    disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
-    register: true,         // register the PWA service worker
-    skipWaiting: true,      // skip waiting for service worker activation
-})(nextConfig);
